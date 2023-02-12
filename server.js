fe89ce27d943f1ecde31a5213076b55591039577
Ruchi Srivastava <ruchisri@microsoft.com>
2023-02-12 19:01:09 +0530

    Added Front End Code

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app=express();
 const port=process.env.PORT || 3030;
 
 app.get("/",function(req,res){
-    res.send("Hello World Again");
+    //res.send("Hello World Again");
+    res.sendFile(__dirname+"/Front-end/index.html");
 })
 app.listen(port,function(){
     console.log("Server running on http://localhost:"+port);
